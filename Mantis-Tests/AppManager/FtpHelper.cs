04fad2d1bbd01398f173e3af1394d0fd763074f4
Alex Sizov <dr.sizov@gmail.com>
2021-12-17 22:49:25 +0300

    initian user creation tests

---
+++ Mantis-Tests/AppManager/FtpHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.FtpClient;
+using System.IO;
+
+namespace Mantis_Tests
+{
+    public class FtpHelper : HelperBase
+    {
+        private FtpClient client;
+        public FtpHelper(ApplicationManager manager) : base(manager)
+        {
+            client = new FtpClient();
+            client.Host = "localhost";
+            client.Credentials = new System.Net.NetworkCredential("mantis", "mantis");
+            client.Connect();
+        }
+        public void BackupFile(String path)
+        {
+            String BackupPath = path + ".bak";
+            if (client.FileExists(BackupPath))
+            {
+                return;
+            }
+            client.Rename(path, BackupPath);
+        }
+        public void RestoreFromBackup(String path)
+        {
+            String BackupPath = path + ".bak";
+            if (!client.FileExists(BackupPath))
+            {
+                return;
+            }
+            if (client.FileExists(path))
+            {
+                client.DeleteFile(path);
+            }
+            client.Rename(BackupPath, path);
+
+        }
+        public void UploadFile(String path, Stream localfile)
+        {
+            if (client.FileExists(path))
+            {
+                client.DeleteFile(path);
+            }
+            using (Stream FtpStream = client.OpenWrite(path))
+            {
+                byte[] buffer = new byte[8 * 1024];
+                int count = localfile.Read(buffer, 0, buffer.Length);
+                while (count > 0)
+                {
+                    FtpStream.Write(buffer, 0, count);
+                    count = localfile.Read(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
---
+++ Mantis-Tests/Tests/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace Mantis_Tests
+{
+    public class TestBase
+    {
+        public static bool Perform_Long_Ui_Checks = true;
+        protected ApplicationManager app;
+
+        [OneTimeSetUp]
+        public void SetupApplicationManager()
+        {
+            app = ApplicationManager.GetInstance();           
+        }
+
+        public static Random rnd = new Random();
+
+        public static string GenerateRandomString(int maxStringLenth)
+        {
+            int lenth = Convert.ToInt32(rnd.NextDouble() * maxStringLenth);
+            StringBuilder builder = new StringBuilder();
+            for (int i=0; i< lenth; i++)
+            {
+                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() * 65)));
+            }
+            return builder.ToString();
+        }
+    }
+} 
